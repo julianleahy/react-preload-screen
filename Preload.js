212fea12f78c6b7156fb33a2789374a01ebe6d6c
julianleahy <julianleahynz@gmail.com>
2018-03-17 15:32:16 +1300

    via props add the Loaded class to remove preloading screen

--- Preload.js
+++ Preload.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './Preload.css'
 
-const Preload = (props) => { 
+const Preload = (props) => {
+  const loader = props.loaded ? 'Loaded' : '';
   return (
+    <div className={loader}>
       <div className="PreloadWrapper">
         <div className="Preload"></div>
         <div className="PreloadSection PreloadLeft"></div>
         <div className="PreloadSection PreloadRight"></div>
+      </div>
     </div>
   )
 }
