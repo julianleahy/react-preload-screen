9471198958e958b15e00d1df59c230d8077ef8ee
julianleahy <julianleahynz@gmail.com>
2018-03-17 15:15:22 +1300

    markup for preloader

---
+++ Preload.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Preload.css'
+
+const Preload = (props) => { 
+  return (
+      <div className="PreloadWrapper">
+        <div className="Preload"></div>
+        <div className="PreloadSection PreloadLeft"></div>
+        <div className="PreloadSection PreloadRight"></div>
+    </div>
+  )
+}
+
+export default Preload;
